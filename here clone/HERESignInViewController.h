6615f06b47eab951632ac2d2bcb39cfe1f3f9ebe
josephktcheung <josephktcheung@gmail.com>
2014-08-06 15:34:16 +0800

    Add sign in view controller

---
+++ here clone/HERESignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  HERESignInViewController.h
+//  here clone
+//
+//  Created by Joseph Cheung on 6/8/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HERESignInViewController : UIViewController
+
+@end
