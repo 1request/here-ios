0b73cbc622498c9055c5058de3b78caedf2692bc
josephktcheung <josephktcheung@gmail.com>
2014-08-11 17:15:39 +0800

    Update home view and add beacons messages view controller

---
+++ here clone/HEREBeaconsMessagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HEREBeaconsMessagesTableViewController.h
+//  here clone
+//
+//  Created by Joseph Cheung on 11/8/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HEREBeaconsMessagesTableViewController : UITableViewController
+
+@end
