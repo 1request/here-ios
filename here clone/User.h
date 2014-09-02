1a96acdefed44ce751d2b01739a92d399f9fe217
Harry Ng <harryworld@gmail.com>
2014-09-02 22:49:22 +0800

    Change user name instead of getting from Parse

---
+++ here clone/User.h
@@ -0,0 +1,15 @@
+//
+//  User.h
+//  here clone
+//
+//  Created by Harry Ng on 2/9/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
++ (NSString *)username;
+
+@end
---
+++ here clone/User.m
@@ -0,0 +1,18 @@
+//
+//  User.m
+//  here clone
+//
+//  Created by Harry Ng on 2/9/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
++ (NSString *)username
+{
+    return @"harry";
+}
+
+@end
