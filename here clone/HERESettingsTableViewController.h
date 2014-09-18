d6ec619133b7c8546f2a361ee2b2142dd60a1f0f
josephktcheung <josephktcheung@gmail.com>
2014-09-18 20:49:08 +0800

    Add SettingsTableViewController

---
+++ here clone/HERESettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HERESettingsTableViewController.h
+//  here clone
+//
+//  Created by Joseph Cheung on 18/9/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HERESettingsTableViewController : UITableViewController
+
+@end
