13bc7c032dedfb4964863e515e24e750b152adf8
josephktcheung <josephktcheung@gmail.com>
2014-09-24 18:37:02 +0800

    Separate managedObjectContext into main and private

--- here clone/AppDelegate.m
+++ here clone/AppDelegate.m
@@ -12,18 +12,16 @@
 #import "APIManager.h"
 #import "HEREMenuTableViewController.h"
 #import "HERELocationHelper.h"
+#import "CoreDataStore.h"
 
 @interface AppDelegate () <locationDelegate>
 
 @property (strong, nonatomic) HERELocationHelper *locationHelper;
-
+@property (strong, nonatomic) NSManagedObjectContext *mainQueueContext;
+@property (strong, nonatomic) NSManagedObjectContext *privateQueueContext;
 @end
 
 @implementation AppDelegate
-
-@synthesize managedObjectContext = _managedObjectContext;
-@synthesize managedObjectModel = _managedObjectModel;
-@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
@@ -52,18 +50,20 @@
     }
     
     HERERootViewController *rootVC = (HERERootViewController *)self.window.rootViewController;
-    UINavigationController *navigationVC = (UINavigationController *)rootVC.contentViewController;
 
-    HEREHomeViewController *homeVC = (HEREHomeViewController *)navigationVC.topViewController;
     HEREMenuTableViewController *menuVC = (HEREMenuTableViewController *)rootVC.menuViewController;
     
-    menuVC.managedObjectContext = self.managedObjectContext;
-    homeVC.managedObjectContext = self.managedObjectContext;
+    self.mainQueueContext = [CoreDataStore mainQueueContext];
+    self.privateQueueContext = [CoreDataStore privateQueueContext];
     
-    [APIManager fetchLocationsWithManagedObjectContext:self.managedObjectContext];
+    menuVC.managedObjectContext = self.mainQueueContext;
+    
+    [APIManager fetchLocationsWithManagedObjectContext:self.privateQueueContext];
     
     self.locationHelper = [[HERELocationHelper alloc] init];
-    self.locationHelper.managedObjectContext = self.managedObjectContext;
+    
+    self.locationHelper.managedObjectContext = self.privateQueueContext;
+    
     self.locationHelper.delegate = self;
     
     [self.locationHelper monitorBeacons];
@@ -98,7 +98,6 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     NSLog(@"applicationWillTerminate");
-    [self saveContext];
 }
 
 #pragma mark - Notifications
@@ -153,105 +152,6 @@
     [[UIApplication sharedApplication] setApplicationIconBadgeNumber: 0];
     [[UIApplication sharedApplication] cancelAllLocalNotifications];
 }
-
-- (void)saveContext
-{
-    NSError *error = nil;
-    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
-    if (managedObjectContext != nil) {
-        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-            abort();
-        }
-    }
-}
-
-#pragma mark - Core Data stack
-
-// Returns the managed object context for the application.
-// If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
-- (NSManagedObjectContext *)managedObjectContext
-{
-    if (_managedObjectContext != nil) {
-        return _managedObjectContext;
-    }
-    
-    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-    if (coordinator != nil) {
-        _managedObjectContext = [[NSManagedObjectContext alloc] init];
-        [_managedObjectContext setPersistentStoreCoordinator:coordinator];
-    }
-    return _managedObjectContext;
-}
-
-// Returns the managed object model for the application.
-// If the model doesn't already exist, it is created from the application's model.
-- (NSManagedObjectModel *)managedObjectModel
-{
-    if (_managedObjectModel != nil) {
-        return _managedObjectModel;
-    }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Here" withExtension:@"momd"];
-    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-    return _managedObjectModel;
-}
-
-// Returns the persistent store coordinator for the application.
-// If the coordinator doesn't already exist, it is created and the application's store added to it.
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
-{
-    if (_persistentStoreCoordinator != nil) {
-        return _persistentStoreCoordinator;
-    }
-    
-    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"Here.sqlite"];
-    
-    NSError *error = nil;
-    
-    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    
-    NSDictionary *options = @{ NSMigratePersistentStoresAutomaticallyOption : @(YES), NSInferMappingModelAutomaticallyOption : @(YES) };
-    
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error]) {
-        /*
-         Replace this implementation with code to handle the error appropriately.
-         
-         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-         
-         Typical reasons for an error here include:
-         * The persistent store is not accessible;
-         * The schema for the persistent store is incompatible with current managed object model.
-         Check the error message to determine what the actual problem was.
-         
-         If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
-         
-         If you encounter schema incompatibility errors during development, you can reduce their frequency by:
-         * Simply deleting the existing store:
-         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
-         
-         * Performing automatic lightweight migration by passing the following dictionary as the options parameter:
-         @{NSMigratePersistentStoresAutomaticallyOption:@YES, NSInferMappingModelAutomaticallyOption:@YES}
-         
-         Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
-         
-         */
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
-    }
-    
-    return _persistentStoreCoordinator;
-}
-
-#pragma mark - Application's Documents directory
-
-// Returns the URL to the application's Documents directory.
-- (NSURL *)applicationDocumentsDirectory
-{
-    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-}
-
 #pragma mark - locationHelper delegate
 
 - (void)notifyWhenEntryBeacon:(CLBeaconRegion *)beaconRegion
